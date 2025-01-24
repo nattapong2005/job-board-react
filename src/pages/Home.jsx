3a08b2d4d78bdfd173c5231925976f6447e5d60e
nattapong <azzzx30089@gmail.com>
2025-01-24 07:19:18 +0700

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,7 +79,7 @@ const Home = () => {
 
       {/* Hot Work Section */}
       <section className="py-10">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-4 py-10">
           <h1 className="font-bold text-lg text-blue-900">Hot work</h1>
           <h2 className="text-3xl font-bold text-gray-800 mb-8">งานที่มาแรง</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
